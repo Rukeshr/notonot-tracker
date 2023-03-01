ed7d717d6f4bbf1e2020c8725b44c158c1b18714
Rukeshr <rukeshr16@gmail.com>
2023-03-01 22:06:46 +0530

    Added task component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,22 @@
 import React from "react";
-import  PropTypes  from 'prop-types';
+// import  PropTypes  from 'prop-types';
 import Button from "./Button";
 
 
 const Header = ({title}) => {
+    const onClick = () => {
+        console.log('click');
+    }
     return (
-        <header className="header">
-            <h1 >{title}</h1>
-            <Button color='green' text='Add' />
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text='Add Task' onClick={onClick} />
         </header>
     )
 }
 
 Header.defaultProps ={
-    title: "notonot tacker"
+    title: "notonot tracker"
 }
 
 // Header.propTypes ={
